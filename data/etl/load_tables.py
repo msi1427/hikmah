bcf74efff12e8ad83f7cb22537cb95bfc1cccd63
msibertman <sabik@leadbook.com>
2021-12-21 08:01:43 +0600

    jsonl to pandas

--- data/etl/load_tables.py
+++ data/etl/load_tables.py
@@ -1,7 +1,22 @@
 import json
+import pandas as pd
 
-def jsonl_to_list(file_path: str):
+def jsonl_to(file_path:str, destination_type:str = "list"):
+    '''
+    Params:
+        file_path: jsonl file path as string
+        destination_type : type of object intended to return
+    Returns:
+        dict_list or pandas dataframe
+    '''
     with open(file_path, 'r') as fp:
         json_list = list(fp)
     dict_list = [json.loads(json_str) for json_str in json_list]
-    return dict_list
+    
+    if destination_type == "list":
+        return dict_list
+    
+    if destination_type == "pandas":
+        keys = list(dict_list[0].keys())
+        df = pd.DataFrame(data=dict_list,columns=keys)
+        return df
